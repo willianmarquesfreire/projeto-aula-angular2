c61c00d9a3a392c6d8b0bb4fd437598fc777ac7b
wmfsystem <willianmarquesfreire@gmail.com>
2017-02-25 14:24:51 -0300

    libs

--- src/app/meu-componente/meu-componente.component.ts
+++ src/app/meu-componente/meu-componente.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-meu-componente',
-  template: `
-    <p #meuNome>Meu nome é {{nome}}.</p>
-  `,
+  templateUrl: './meu-componente.component.html',
   styleUrls: ['./meu-componente.component.css']
 })
 export class MeuComponenteComponent implements OnInit {
